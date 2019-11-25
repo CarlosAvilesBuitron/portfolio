e93ba8dbc1bd553c78ed09b2dfc3a03867c106d6
Carlos Aviles Buitron <c.avilesbuitron#gmail.com>
2019-11-25 00:31:19 +0000

    Updated resume files

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,10 +14,10 @@ function Footer(){
             <p><span className="highlight">&copy;2019</span> Carlos Aviles Buitron</p>
             <div id="footer_social_icons">
                 Find me on:
-                <a href="https://www.facebook.com/carlos.avilesbuitron.3" rel="noopener noreferrer" target="_blank"><FaFacebook/></a>
-                <a href="https://www.instagram.com/carlos.socializes/" rel="noopener noreferrer" target="_blank"><FaInstagram/></a>
                 <a href="https://github.com/CarlosAvilesBuitron" rel="noopener noreferrer" target="_blank"><FaGithub/></a>
                 <a href="https://www.linkedin.com/in/carlosavilesbuitron/" rel="noopener noreferrer" target="_blank"><FaLinkedin/></a>
+                <a href="https://www.facebook.com/carlos.avilesbuitron.3" rel="noopener noreferrer" target="_blank"><FaFacebook/></a>
+                <a href="https://www.instagram.com/carlos.socializes/" rel="noopener noreferrer" target="_blank"><FaInstagram/></a>
             </div>
         </footer>
     );
--- src/components/social.js
+++ src/components/social.js
@@ -12,10 +12,10 @@ import {
 function SocialIcons(){
     return(
         <div id="social_icons">
-            <a href="https://www.facebook.com/carlos.avilesbuitron.3" rel="noopener noreferrer" target="_blank"><FaFacebook/></a>
-            <a href="https://www.instagram.com/carlos.socializes/" rel="noopener noreferrer" target="_blank"><FaInstagram/></a>
             <a href="https://github.com/CarlosAvilesBuitron" rel="noopener noreferrer" target="_blank"><FaGithub/></a>
             <a href="https://www.linkedin.com/in/carlosavilesbuitron/" rel="noopener noreferrer" target="_blank"><FaLinkedin/></a>
+            <a href="https://www.facebook.com/carlos.avilesbuitron.3" rel="noopener noreferrer" target="_blank"><FaFacebook/></a>
+            <a href="https://www.instagram.com/carlos.socializes/" rel="noopener noreferrer" target="_blank"><FaInstagram/></a>
         </div>
     );
 }
